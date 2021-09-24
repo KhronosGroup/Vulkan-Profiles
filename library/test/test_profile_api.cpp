6034ce0d9cc3a7a2ee45a76ffa1da0fcb0faa216
Christophe <christophe@lunarg.com>
2021-09-24 17:06:26 +0200

    Library: factorize utility test code

--- library/test/test_profile_api.cpp
+++ library/test/test_profile_api.cpp
@@ -1,33 +1,30 @@
-#include "vulkan_profiles.hpp"
+/*
+ * Copyright (c) 2021-2022 Valve Corporation
+ * Copyright (c) 2021-2022 LunarG, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors:
+ * - Christophe Riccio <christophe@lunarg.com>
+ */
 
-#include <gtest/gtest.h>
+#include "test.hpp"
+
+#include "vulkan_profiles.hpp"
 
 #include <cstdio>
 #include <memory>
 #include <vector>
-
-#if defined(_WIN32)
-#include <windows.h>
-#include <crtdbg.h>
-#include <errhandlingapi.h>
-#endif
-
-struct DisableDebugPopup {
-    DisableDebugPopup() {
-#if defined(_WIN32)
-#if !defined(NDEBUG)
-        _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-        _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-#endif
-        // Avoid "Abort, Retry, Ignore" dialog boxes
-        _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
-        SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
-        _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-        _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-#endif
-    }
-};
-static const DisableDebugPopup disable_debug_popup;
 
 static const float DEFAULT_QUEUE_PRIORITY(0.0f);
 
