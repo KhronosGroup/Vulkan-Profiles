8f24ff1204e431fc0be68e185e85aea6a4bc5a7e
Christophe <christophe@lunarg.com>
2022-01-26 18:57:57 +0100

    layer: improve unit test execution time

--- library/test/test_profile_examples.cpp
+++ library/test/test_profile_examples.cpp
@@ -20,9 +20,9 @@
 
 #include "test.hpp"
 #ifndef VULKAN_PROFILES_HEADER_ONLY
-#include <vulkan/vulkan_profiles.hpp>
+#include <vulkan/debug/vulkan_profiles.hpp>
 #else
-#include <vulkan/vulkan_profiles.h>
+#include <vulkan/debug/vulkan_profiles.h>
 #endif
 
 #define countof(arr) sizeof(arr) / sizeof(arr[0])
