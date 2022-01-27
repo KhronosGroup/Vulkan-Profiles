1a74c27b0b2b55e9fa8b553833273039914f7b7c
Mark Lobodzinski <mark@lunarg.com>
2022-01-27 13:30:11 -0700

    profile tests: Add android-specific test and APK build

--- library/test/test_api_create_device_android.cpp
+++ library/test/test_api_create_device_android.cpp
@@ -16,6 +16,7 @@
  *
  * Authors:
  * - Christophe Riccio <christophe@lunarg.com>
+ *   Mark Lobodzinski <mark@lunarg.com>
  */
 
 #define VK_ENABLE_BETA_EXTENSIONS 1
@@ -23,6 +24,233 @@
 #include "test.hpp"
 #include <vulkan/vulkan_android.h>
 #include <vulkan/debug/vulkan_profiles.hpp>
+
+#if defined(ANDROID)
+#include <android/native_window.h>
+#include <android/log.h>
+#include <android_native_app_glue.h>
+
+const char *appTag = "VpLibrary_test_api_create_device_android";
+static bool initialized = false;
+static bool active = false;
+static android_app *g_app;
+
+static TestScaffold* scaffold = nullptr;
+
+void addFullTestCommentIfPresent(const ::testing::TestInfo &test_info, std::string &error_message) {
+    const char *const type_param = test_info.type_param();
+    const char *const value_param = test_info.value_param();
+
+    if (type_param != NULL || value_param != NULL) {
+        error_message.append(", where ");
+        if (type_param != NULL) {
+            error_message.append("TypeParam = ").append(type_param);
+            if (value_param != NULL) error_message.append(" and ");
+        }
+        if (value_param != NULL) {
+            error_message.append("GetParam() = ").append(value_param);
+        }
+    }
+}
+
+// Inspired by https://github.com/google/googletest/blob/master/googletest/docs/AdvancedGuide.md
+class LogcatPrinter : public ::testing::EmptyTestEventListener {
+    // Called before a test starts.
+    virtual void OnTestStart(const ::testing::TestInfo &test_info) {
+        __android_log_print(ANDROID_LOG_INFO, appTag, "[ RUN      ] %s.%s", test_info.test_case_name(), test_info.name());
+    }
+
+    // Called after a failed assertion or a SUCCEED() invocation.
+    virtual void OnTestPartResult(const ::testing::TestPartResult &result) {
+        // If the test part succeeded, we don't need to do anything.
+        if (result.type() == ::testing::TestPartResult::kSuccess) return;
+
+        __android_log_print(ANDROID_LOG_INFO, appTag, "%s in %s:%d %s", result.failed() ? "*** Failure" : "Success",
+                            result.file_name(), result.line_number(), result.summary());
+    }
+
+    // Called after a test ends.
+    virtual void OnTestEnd(const ::testing::TestInfo &info) {
+        std::string result;
+        if (info.result()->Passed()) {
+            result.append("[       OK ]");
+        } else {
+            result.append("[  FAILED  ]");
+        }
+        result.append(info.test_case_name()).append(".").append(info.name());
+        if (info.result()->Failed()) addFullTestCommentIfPresent(info, result);
+
+        if (::testing::GTEST_FLAG(print_time)) {
+            std::ostringstream os;
+            os << info.result()->elapsed_time();
+            result.append(" (").append(os.str()).append(" ms)");
+        }
+
+        __android_log_print(ANDROID_LOG_INFO, appTag, "%s", result.c_str());
+    };
+};
+
+// Convert Intents to argv
+std::vector<std::string> get_args(android_app &app, const char *intent_extra_data_key) {
+    std::vector<std::string> args;
+    JavaVM &vm = *app.activity->vm;
+    JNIEnv *p_env;
+    if (vm.AttachCurrentThread(&p_env, nullptr) != JNI_OK) return args;
+
+    JNIEnv &env = *p_env;
+    jobject activity = app.activity->clazz;
+    jmethodID get_intent_method = env.GetMethodID(env.GetObjectClass(activity), "getIntent", "()Landroid/content/Intent;");
+    jobject intent = env.CallObjectMethod(activity, get_intent_method);
+    jmethodID get_string_extra_method =
+        env.GetMethodID(env.GetObjectClass(intent), "getStringExtra", "(Ljava/lang/String;)Ljava/lang/String;");
+    jvalue get_string_extra_args;
+    get_string_extra_args.l = env.NewStringUTF(intent_extra_data_key);
+    jstring extra_str = static_cast<jstring>(env.CallObjectMethodA(intent, get_string_extra_method, &get_string_extra_args));
+
+    std::string args_str;
+    if (extra_str) {
+        const char *extra_utf = env.GetStringUTFChars(extra_str, nullptr);
+        args_str = extra_utf;
+        env.ReleaseStringUTFChars(extra_str, extra_utf);
+        env.DeleteLocalRef(extra_str);
+    }
+
+    env.DeleteLocalRef(get_string_extra_args.l);
+    env.DeleteLocalRef(intent);
+    vm.DetachCurrentThread();
+
+    // split args_str
+    std::stringstream ss(args_str);
+    std::string arg;
+    while (std::getline(ss, arg, ' ')) {
+        if (!arg.empty()) args.push_back(arg);
+    }
+
+    return args;
+}
+
+static int32_t processInput(struct android_app *app, AInputEvent *event) { return 0; }
+
+static void processCommand(struct android_app *app, int32_t cmd) {
+    switch (cmd) {
+        case APP_CMD_INIT_WINDOW: {
+            if (app->window) {
+                initialized = true;
+            }
+            break;
+        }
+        case APP_CMD_GAINED_FOCUS: {
+            active = true;
+            break;
+        }
+        case APP_CMD_LOST_FOCUS: {
+            active = false;
+            break;
+        }
+    }
+}
+
+static void destroyActivity(struct android_app *app) {
+    ANativeActivity_finish(app->activity);
+
+    // Wait for APP_CMD_DESTROY
+    while (app->destroyRequested == 0) {
+        struct android_poll_source *source = nullptr;
+        int events = 0;
+        int result = ALooper_pollAll(-1, nullptr, &events, reinterpret_cast<void **>(&source));
+
+        if ((result >= 0) && (source)) {
+            source->process(app, source);
+        } else {
+            break;
+        }
+    }
+}
+
+void android_main(struct android_app *app) {
+    static ANativeWindow *window;
+    app->onAppCmd = processCommand;
+    app->onInputEvent = processInput;
+    g_app = app;
+
+    while (1) {
+        int events;
+        struct android_poll_source *source;
+        while (ALooper_pollAll(active ? 0 : -1, NULL, &events, (void **)&source) >= 0) {
+            if (source) {
+                source->process(app, source);
+            }
+
+            if (app->destroyRequested != 0) {
+                return;
+            }
+        }
+
+        if (initialized && active) {
+            // Use the following key to send arguments to gtest, i.e.
+            // --es args "--gtest_filter=-VkLayerTest.foo"
+            const char key[] = "args";
+            std::vector<std::string> args = get_args(*app, key);
+
+            std::string filter = "";
+            if (args.size() > 0) {
+                __android_log_print(ANDROID_LOG_INFO, appTag, "Intent args = %s", args[0].c_str());
+                filter += args[0];
+            } else {
+                __android_log_print(ANDROID_LOG_INFO, appTag, "No Intent args detected");
+            }
+
+            int argc = 2;
+            char *argv[] = {(char *)"foo", (char *)filter.c_str()};
+            __android_log_print(ANDROID_LOG_DEBUG, appTag, "filter = %s", argv[1]);
+
+            // Route output to files until we can override the gtest output
+            freopen("/sdcard/Android/data/com.example.VulkanProfilesLayerTests/files/out.txt", "w", stdout);
+            freopen("/sdcard/Android/data/com.example.VulkanProfilesLayerTests/files/err.txt", "w", stderr);
+
+            ::testing::InitGoogleTest(&argc, argv);
+
+            ::testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+            listeners.Append(new LogcatPrinter);
+
+            ::scaffold = new TestScaffold;
+
+            int result = RUN_ALL_TESTS();
+
+            if (result != 0) {
+                __android_log_print(ANDROID_LOG_INFO, appTag, "==== Tests FAILED ====");
+            } else {
+                __android_log_print(ANDROID_LOG_INFO, appTag, "==== Tests PASSED ====");
+            }
+
+            fclose(stdout);
+            fclose(stderr);
+
+            destroyActivity(app);
+            delete ::scaffold;
+            ::scaffold = nullptr;
+            raise(SIGTERM);
+            return;
+        }
+    }
+}
+#else
+
+int main(int argc, char **argv) {
+    int result;
+    ::testing::InitGoogleTest(&argc, argv);
+
+    ::scaffold = new TestScaffold;
+
+    result = RUN_ALL_TESTS();
+
+    delete ::scaffold;
+    ::scaffold = nullptr;
+
+    return result;
+}
+
+#endif
 
 TEST(library_api, vpCreateDevice) {
     TestScaffold scaffold;
@@ -44,7 +272,7 @@ TEST(library_api, vpCreateDevice) {
 
     VkDevice device = VK_NULL_HANDLE;
     VkResult res = vpCreateDevice(scaffold.physicalDevice, &profileInfo, nullptr, &device);
-#ifdef __ANDROID_API__
+#if defined(ANDROID)
     EXPECT_TRUE(res == VK_SUCCESS);
     EXPECT_TRUE(device != VK_NULL_HANDLE);
 #else
@@ -60,7 +288,7 @@ TEST(library_api, vpGetPhysicalDeviceProfileSupport) {
     VkBool32 supported = VK_FALSE;
     vpGetPhysicalDeviceProfileSupport(scaffold.instance, scaffold.physicalDevice, &profile, &supported);
 
-#ifdef __ANDROID_API__
+#if defined(ANDROID)
     EXPECT_EQ(VK_TRUE, supported);
 #else
     EXPECT_EQ(VK_FALSE, supported);
