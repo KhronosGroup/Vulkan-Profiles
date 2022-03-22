6c918a105071ee9cd2ac10960f0812550d219624
Christophe <christophe@lunarg.com>
2022-03-22 14:01:10 +0100

    cmake: Vulkan Loader has a bug on master branch"

--- scripts/known_good.cmake
+++ scripts/known_good.cmake
@@ -4,7 +4,7 @@ set(GIT_VULKAN_HEADER_COMMIT v1.3.208)
 
 # Vulkan Loader
 set(GIT_VULKAN_LOADER_REPOSITORY https://github.com/KhronosGroup/Vulkan-Loader.git)
-set(GIT_VULKAN_LOADER_COMMIT v1.3.208)
+set(GIT_VULKAN_LOADER_COMMIT sdk-1.3.204)
 
 # Vulkan Validation Layer
 set(GIT_VULKAN_VALIDATION_REPOSITORY https://github.com/KhronosGroup/Vulkan-ValidationLayers)
