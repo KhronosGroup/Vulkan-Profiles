439639f814aabb017b557ee533decb8f7ed2b161
Christophe <christophe@lunarg.com>
2022-03-31 18:04:22 +0200

    Update to 209 header

--- scripts/known_good.cmake
+++ scripts/known_good.cmake
@@ -1,6 +1,6 @@
 # Vulkan Headers
 set(GIT_VULKAN_HEADER_REPOSITORY https://github.com/KhronosGroup/Vulkan-Headers)
-set(GIT_VULKAN_HEADER_COMMIT v1.3.208)
+set(GIT_VULKAN_HEADER_COMMIT v1.3.209)
 
 # Vulkan Loader
 set(GIT_VULKAN_LOADER_REPOSITORY https://github.com/KhronosGroup/Vulkan-Loader.git)
@@ -8,5 +8,5 @@ set(GIT_VULKAN_LOADER_COMMIT sdk-1.3.204)
 
 # Vulkan Validation Layer
 set(GIT_VULKAN_VALIDATION_REPOSITORY https://github.com/KhronosGroup/Vulkan-ValidationLayers)
-set(GIT_VULKAN_VALIDATION_COMMIT v1.3.208)
+set(GIT_VULKAN_VALIDATION_COMMIT v1.3.209)
 
