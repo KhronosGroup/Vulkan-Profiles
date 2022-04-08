4cb10516c1dc0dba3b03417b0d4464158756c87b
Lava Block <the@lava-block.com>
2022-04-08 13:53:31 +0200

    script: build library after fixes

--- library/source/debug/vulkan_profiles.cpp
+++ library/source/debug/vulkan_profiles.cpp
@@ -7888,8 +7888,6 @@ VPAPI_ATTR VkResult vpCreateInstance(const VpInstanceCreateInfo *pCreateInfo,
     VkApplicationInfo appInfo{ VK_STRUCTURE_TYPE_APPLICATION_INFO };
     std::vector<const char*> extensions;
     VkInstanceCreateInfo* pInstanceCreateInfo = nullptr;
-    VkExtensionProperties* pProfileExtensions = nullptr;
-    uint32_t profileExtensionCount = 0;
 
     if (pCreateInfo != nullptr && pCreateInfo->pCreateInfo != nullptr) {
         createInfo = *pCreateInfo->pCreateInfo;
@@ -8501,8 +8499,6 @@ VPAPI_ATTR VkResult vpGetProfileFormats(const VpProfileProperties *pProfile, uin
 }
 
 VPAPI_ATTR void vpGetProfileFormatProperties(const VpProfileProperties *pProfile, VkFormat format, void *pNext) {
-    VkResult result = VK_SUCCESS;
-
     const detail::VpProfileDesc* pDesc = detail::vpGetProfileDesc(pProfile->profileName);
     if (pDesc == nullptr) return;
 
