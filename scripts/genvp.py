85afd1c18d1bcb7688b4aad1313732aefdbfa494
Christophe <christophe@lunarg.com>
2022-05-18 16:33:28 +0200

    profiles: add sampleLocationSubPixelBits workaround

--- scripts/genvp.py
+++ scripts/genvp.py
@@ -1974,6 +1974,9 @@ class VulkanRegistry():
             self.structs['VkPhysicalDeviceFragmentDensityMap2PropertiesEXT'].members['subsampledLoads'].limittype = 'behavior'
             self.structs['VkPhysicalDeviceFragmentDensityMap2PropertiesEXT'].members['subsampledCoarseReconstructionEarlyAccess'].limittype = 'behavior'
 
+        if 'VkPhysicalDeviceSampleLocationsPropertiesEXT' in self.structs:
+            self.structs['VkPhysicalDeviceSampleLocationsPropertiesEXT'].members['sampleLocationSubPixelBits'].limittype = 'max' # should be 'bits'
+
         if 'VkPhysicalDeviceRayTracingPropertiesNV' in self.structs:
             self.structs['VkPhysicalDeviceRayTracingPropertiesNV'].members['shaderGroupHandleSize'].limittype = 'behavior'
             self.structs['VkPhysicalDeviceRayTracingPropertiesNV'].members['shaderGroupBaseAlignment'].limittype = 'behavior'
