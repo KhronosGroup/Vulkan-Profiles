0deec43aae63010ad60d839229344bc8ddeb465c
Christophe <christophe@lunarg.com>
2022-07-19 16:02:02 +0200

    merge: fix noauto member and unit tests

--- layer/tests/tests_combine_union.cpp
+++ layer/tests/tests_combine_union.cpp
@@ -22,7 +22,8 @@
 #include <gtest/gtest.h>
 #include "profiles_test_helper.h"
 
-static VkPhysicalDevice gpu = VK_NULL_HANDLE;
+static VkPhysicalDevice gpu_native = VK_NULL_HANDLE;
+static VkPhysicalDevice gpu_profile = VK_NULL_HANDLE;
 static profiles_test::VulkanInstanceBuilder inst_builder;
 
 class TestsUnion : public VkTestFramework {
@@ -44,7 +45,10 @@ class TestsUnion : public VkTestFramework {
         err = inst_builder.init(&settings);
         ASSERT_EQ(err, VK_SUCCESS);
 
-        err = inst_builder.getPhysicalDevice(profiles_test::MODE_PROFILE, &gpu);
+        err = inst_builder.getPhysicalDevice(profiles_test::MODE_PROFILE, &gpu_profile);
+        ASSERT_EQ(err, VK_SUCCESS);
+
+        err = inst_builder.getPhysicalDevice(profiles_test::MODE_NATIVE, &gpu_native);
         ASSERT_EQ(err, VK_SUCCESS);
     }
 
@@ -55,9 +59,9 @@ class TestsUnion : public VkTestFramework {
 
 TEST_F(TestsUnion, Extension) {
     uint32_t count;
-    vkEnumerateDeviceExtensionProperties(gpu, nullptr, &count, nullptr);
+    vkEnumerateDeviceExtensionProperties(gpu_profile, nullptr, &count, nullptr);
     std::vector<VkExtensionProperties> device_extensions(count);
-    vkEnumerateDeviceExtensionProperties(gpu, nullptr, &count, device_extensions.data());
+    vkEnumerateDeviceExtensionProperties(gpu_profile, nullptr, &count, device_extensions.data());
 
     ASSERT_STREQ("VK_KHR_maintenance1", device_extensions[0].extensionName);
     ASSERT_STREQ("VK_KHR_maintenance2", device_extensions[1].extensionName);
@@ -67,7 +71,7 @@ TEST_F(TestsUnion, Extension) {
 
 TEST_F(TestsUnion, Feature) {
     VkPhysicalDeviceFeatures gpu_features{};
-    vkGetPhysicalDeviceFeatures(gpu, &gpu_features);
+    vkGetPhysicalDeviceFeatures(gpu_profile, &gpu_features);
 
     EXPECT_EQ(gpu_features.depthBiasClamp, VK_TRUE);
     EXPECT_EQ(gpu_features.depthClamp, VK_FALSE);
@@ -81,7 +85,36 @@ TEST_F(TestsUnion, Feature) {
 
 TEST_F(TestsUnion, Properties) {
     VkPhysicalDeviceProperties gpu_props{};
-    vkGetPhysicalDeviceProperties(gpu, &gpu_props);
+    vkGetPhysicalDeviceProperties(gpu_profile, &gpu_props);
 
-    //EXPECT_EQ(gpu_props.limits.maxImageDimension1D, 4096u);
+    EXPECT_EQ(gpu_props.limits.maxImageDimension1D, 16384u);
+    EXPECT_EQ(gpu_props.limits.maxImageDimension2D, 8192u);
+    EXPECT_EQ(gpu_props.limits.maxComputeWorkGroupCount[0], 65535u);
+    EXPECT_EQ(gpu_props.limits.maxComputeWorkGroupCount[1], 65535u);
+    EXPECT_EQ(gpu_props.limits.maxComputeWorkGroupCount[2], 4096u);
+    EXPECT_EQ(gpu_props.limits.mipmapPrecisionBits, 4u);
+    EXPECT_TRUE(std::abs(gpu_props.limits.maxSamplerAnisotropy - 16.0) < 0.0001);
+    EXPECT_EQ(gpu_props.limits.viewportBoundsRange[0], -32768);
+    EXPECT_EQ(gpu_props.limits.viewportBoundsRange[1], 32767);
+    EXPECT_EQ(gpu_props.limits.minMemoryMapAlignment, 2048);
+    EXPECT_EQ(gpu_props.limits.minTexelOffset, -8);
+    EXPECT_EQ(gpu_props.limits.maxTexelOffset, 7);
+    EXPECT_EQ(gpu_props.limits.framebufferColorSampleCounts, VK_SAMPLE_COUNT_1_BIT | VK_SAMPLE_COUNT_2_BIT | VK_SAMPLE_COUNT_4_BIT);
+    EXPECT_EQ(gpu_props.limits.pointSizeRange[0], 2.0);
+    EXPECT_EQ(gpu_props.limits.pointSizeRange[1], 64.0);
+    EXPECT_EQ(gpu_props.limits.pointSizeGranularity, 2.0);
+}
+
+// Noauto limittype member should not be modified
+TEST_F(TestsUnion, Noauto) {
+    VkPhysicalDeviceProperties gpu_props_native{};
+    vkGetPhysicalDeviceProperties(gpu_native, &gpu_props_native);
+
+    VkPhysicalDeviceProperties gpu_props_profile{};
+    vkGetPhysicalDeviceProperties(gpu_profile, &gpu_props_profile);
+
+    EXPECT_EQ(gpu_props_profile.deviceID, gpu_props_native.deviceID);
+    EXPECT_EQ(gpu_props_profile.vendorID, gpu_props_native.vendorID);
+    EXPECT_STREQ(gpu_props_profile.deviceName, gpu_props_native.deviceName);
+    EXPECT_EQ(gpu_props_profile.deviceType, gpu_props_native.deviceType);
 }
