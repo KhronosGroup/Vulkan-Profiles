9cb679b6d62d37415db1a84f0ffbb3b7db1c915c
Christophe <christophe@lunarg.com>
2022-09-07 16:01:17 +0200

    profiles: clean up CMake

---
+++ generator/generator.cpp
@@ -0,0 +1,19 @@
+#include "generator.hpp"
+
+#include <json/json.h>  // https://github.com/open-source-parsers/jsoncpp
+
+#include <valijson/adapters/jsoncpp_adapter.hpp>
+#include <valijson/schema.hpp>
+#include <valijson/schema_parser.hpp>
+#include <valijson/validation_results.hpp>
+#include <valijson/validator.hpp>
+
+using valijson::Schema;
+using valijson::SchemaParser;
+using valijson::ValidationResults;
+using valijson::Validator;
+using valijson::adapters::JsonCppAdapter;
+
+int main(int argc, char argv[]) { 
+	return 0; 
+}
