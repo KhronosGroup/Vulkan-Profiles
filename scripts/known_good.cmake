6ef6c9ca957367b127a5aab9ef393b47ef275ab1
ziga-lunarg <ziga@lunarg.com>
2022-09-23 12:18:40 +0200

    profiles: Update to 229 header

--- scripts/known_good.cmake
+++ scripts/known_good.cmake
@@ -1,12 +1,12 @@
 # Vulkan Headers
 set(GIT_VULKAN_HEADER_REPOSITORY https://github.com/KhronosGroup/Vulkan-Headers)
-set(GIT_VULKAN_HEADER_COMMIT v1.3.228)
+set(GIT_VULKAN_HEADER_COMMIT v1.3.229)
 
 # Vulkan Loader
 set(GIT_VULKAN_LOADER_REPOSITORY https://github.com/KhronosGroup/Vulkan-Loader.git)
-set(GIT_VULKAN_LOADER_COMMIT v1.3.228)
+set(GIT_VULKAN_LOADER_COMMIT v1.3.229)
 
 # Vulkan Validation Layer
 set(GIT_VULKAN_VALIDATION_REPOSITORY https://github.com/KhronosGroup/Vulkan-ValidationLayers)
-set(GIT_VULKAN_VALIDATION_COMMIT v1.3.228)
+set(GIT_VULKAN_VALIDATION_COMMIT v1.3.229)
 
