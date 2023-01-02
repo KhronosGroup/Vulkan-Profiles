95729eee559eba265660d60471fa2a034e1b8fd5
Christophe <christophe@lunarg.com>
2023-01-02 16:41:18 +0100

    profile: add Android Baseline 2022 v1 and 2021 v2 profiles

--- profiles/test/test_validate.cpp
+++ profiles/test/test_validate.cpp
@@ -240,3 +240,10 @@ TEST(test_validate, VP_ANDROID_baseline_2021) {
     const Json::Value json_document3 = ParseJsonFile("VP_ANDROID_baseline_2021.json");
     EXPECT_TRUE(validator.Check(json_document3));
 }
+
+TEST(test_validate, VP_ANDROID_baseline_2022) {
+    JsonValidator validator;
+
+    const Json::Value json_document3 = ParseJsonFile("VP_ANDROID_baseline_2022.json");
+    EXPECT_TRUE(validator.Check(json_document3));
+}
