ff1b1c30dadcd7d06b45e198becd8499a98c6935
Christophe <christophe@lunarg.com>
2023-01-16 19:03:01 +0100

    Updated copyright date

--- layer/tests/tests_mechanism.cpp
+++ layer/tests/tests_mechanism.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2021-2022 Valve Corporation
- * Copyright (C) 2021-2022 LunarG, Inc.
+ * Copyright (C) 2021-2023 Valve Corporation
+ * Copyright (C) 2021-2023 LunarG, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
