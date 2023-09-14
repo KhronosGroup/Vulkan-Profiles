abab269027b6d72951d4a21142ba63dba3761671
Christophe <christophe@lunarg.com>
2023-09-14 16:37:05 +0200

    utility: Update to use vku namespace instead of vul

--- layer/vk_layer_table.cpp
+++ layer/vk_layer_table.cpp
@@ -28,17 +28,17 @@
 static device_table_map tableMap;
 static instance_table_map tableInstanceMap;
 
-dispatch_key get_dispatch_key(const void *object) { return (dispatch_key) * (VulDeviceDispatchTable **)object; }
+dispatch_key get_dispatch_key(const void *object) { return (dispatch_key) * (VkuDeviceDispatchTable **)object; }
 
 // Map lookup must be thread safe
-VulDeviceDispatchTable *device_dispatch_table(void *object) {
+VkuDeviceDispatchTable *device_dispatch_table(void *object) {
     dispatch_key key = get_dispatch_key(object);
     device_table_map::const_iterator it = tableMap.find((void *)key);
     assert(it != tableMap.end() && "Not able to find device dispatch entry");
     return it->second.get();
 }
 
-VulInstanceDispatchTable *instance_dispatch_table(void *object) {
+VkuInstanceDispatchTable *instance_dispatch_table(void *object) {
     dispatch_key key = get_dispatch_key(object);
     instance_table_map::const_iterator it = tableInstanceMap.find((void *)key);
     assert(it != tableInstanceMap.end() && "Not able to find instance dispatch entry");
@@ -63,14 +63,14 @@ void destroy_device_dispatch_table(dispatch_key key) { destroy_dispatch_table(ta
 
 void destroy_instance_dispatch_table(dispatch_key key) { destroy_dispatch_table(tableInstanceMap, key); }
 
-VulDeviceDispatchTable *get_dispatch_table(device_table_map &map, void *object) {
+VkuDeviceDispatchTable *get_dispatch_table(device_table_map &map, void *object) {
     dispatch_key key = get_dispatch_key(object);
     device_table_map::const_iterator it = map.find((void *)key);
     assert(it != map.end() && "Not able to find device dispatch entry");
     return it->second.get();
 }
 
-VulInstanceDispatchTable *get_dispatch_table(instance_table_map &map, void *object) {
+VkuInstanceDispatchTable *get_dispatch_table(instance_table_map &map, void *object) {
     dispatch_key key = get_dispatch_key(object);
     instance_table_map::const_iterator it = map.find((void *)key);
     assert(it != map.end() && "Not able to find instance dispatch entry");
@@ -102,20 +102,20 @@ VkLayerDeviceCreateInfo *get_chain_info(const VkDeviceCreateInfo *pCreateInfo, V
  *    Device -> CommandBuffer or Queue
  * If use the object themselves as key to map then implies Create entrypoints have to be intercepted
  * and a new key inserted into map */
-VulInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa, instance_table_map &map) {
-    VulInstanceDispatchTable *pTable;
+VkuInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa, instance_table_map &map) {
+    VkuInstanceDispatchTable *pTable;
     dispatch_key key = get_dispatch_key(instance);
     instance_table_map::const_iterator it = map.find((void *)key);
 
     if (it == map.end()) {
-        auto table = std::make_unique<VulInstanceDispatchTable>();
+        auto table = std::make_unique<VkuInstanceDispatchTable>();
         pTable = table.get();
         map[(void *)key] = std::move(table);
     } else {
         return it->second.get();
     }
 
-    vulInitInstanceDispatchTable(instance, pTable, gpa);
+    vkuInitInstanceDispatchTable(instance, pTable, gpa);
 
     // Setup func pointers that are required but not externally exposed.  These won't be added to the instance dispatch table by
     // default.
@@ -124,29 +124,29 @@ VulInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGet
     return pTable;
 }
 
-VulInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa) {
+VkuInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa) {
     return initInstanceTable(instance, gpa, tableInstanceMap);
 }
 
-VulDeviceDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa, device_table_map &map) {
-    VulDeviceDispatchTable *pTable;
+VkuDeviceDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa, device_table_map &map) {
+    VkuDeviceDispatchTable *pTable;
     dispatch_key key = get_dispatch_key(device);
     device_table_map::const_iterator it = map.find((void *)key);
 
     if (it == map.end()) {
-        auto table = std::make_unique<VulDeviceDispatchTable>();
+        auto table = std::make_unique<VkuDeviceDispatchTable>();
         pTable = table.get();
         map[(void *)key] = std::move(table);
     } else {
         return it->second.get();
     }
 
-    vulInitDeviceDispatchTable(device, pTable, gpa);
+    vkuInitDeviceDispatchTable(device, pTable, gpa);
 
     return pTable;
 }
 
-VulDeviceDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa) {
+VkuDeviceDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa) {
     return initDeviceTable(device, gpa, tableMap);
 }
 
