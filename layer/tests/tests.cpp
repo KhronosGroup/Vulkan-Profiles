03cf400beaf0eea42688da1a288f0eb1c9d2be6b
Christophe <christophe@lunarg.com>
2024-02-21 20:13:25 +0100

    Add merge script JSON config file using the new `--config` argument

--- layer/tests/tests.cpp
+++ layer/tests/tests.cpp
@@ -42,7 +42,7 @@ class LayerTests : public VkTestFramework {
 TEST_F(LayerTests, TestDesktop2023) {
     TEST_DESCRIPTION("Test specifying device extensions to be excluded from being reported by the device");
 
-    static const char* profile_file_data = JSON_PROFILES_PATH "VP_LUNARG_desktop_baseline_2023.json";
+    static const char* profile_file_data = JSON_PROFILES_PATH "VP_LUNARG_desktop_baseline.json";
     static const char* profile_name_data = "VP_LUNARG_desktop_baseline_2023";
     VkBool32 emulate_portability_data = VK_TRUE;
     VkBool32 debug_fail_on_error = VK_FALSE;
@@ -267,7 +267,7 @@ TEST_F(LayerTests, TestExcludingDeviceExtensions) {
 
     VkResult err = VK_SUCCESS;
 
-    const char* profile_file_data = JSON_PROFILES_PATH "VP_LUNARG_desktop_baseline_2023.json";
+    const char* profile_file_data = JSON_PROFILES_PATH "VP_LUNARG_desktop_baseline.json";
     const char* profile_name_data = "VP_LUNARG_desktop_baseline_2023";
     VkBool32 emulate_portability_data = VK_TRUE;
     VkBool32 debug_fail_on_error = VK_FALSE;
@@ -332,7 +332,7 @@ TEST_F(LayerTests, TestExcludingDeviceExtensions) {
 TEST_F(LayerTests, TestExcludingFormats) {
     TEST_DESCRIPTION("Test specifying image formats to be excluded from being reported by the device");
 
-    const char* profile_file_data = JSON_PROFILES_PATH "VP_LUNARG_desktop_baseline_2023.json";
+    const char* profile_file_data = JSON_PROFILES_PATH "VP_LUNARG_desktop_baseline.json";
     VkBool32 emulate_portability_data = VK_TRUE;
     VkBool32 debug_fail_on_error = VK_TRUE;
     const std::vector<const char*> exclude_formats = {"VK_FORMAT_R8G8B8A8_UNORM"};
--- profiles/test/test_validate.cpp
+++ profiles/test/test_validate.cpp
@@ -247,26 +247,10 @@ TEST(test_validate, VP_LUNARG_minimum_requirements) {
     EXPECT_TRUE(validator.Check(json_document2));
 }
 
-TEST(test_validate, VP_LUNARG_desktop_baseline_2022) {
+TEST(test_validate, VP_LUNARG_desktop_baseline) {
     JsonValidator validator;
 
-    const std::string path = std::string(PROFILE_FILES_PATH) + "VP_LUNARG_desktop_baseline_2022.json";
-    const Json::Value json_document2 = ParseJsonFile(path.c_str());
-    EXPECT_TRUE(validator.Check(json_document2));
-}
-
-TEST(test_validate, VP_LUNARG_desktop_baseline_2023) {
-    JsonValidator validator;
-
-    const std::string path = std::string(PROFILE_FILES_PATH) + "VP_LUNARG_desktop_baseline_2023.json";
-    const Json::Value json_document2 = ParseJsonFile(path.c_str());
-    EXPECT_TRUE(validator.Check(json_document2));
-}
-
-TEST(test_validate, VP_LUNARG_desktop_baseline_2024) {
-    JsonValidator validator;
-
-    const std::string path = std::string(PROFILE_FILES_PATH) + "VP_LUNARG_desktop_baseline_2024.json";
+    const std::string path = std::string(PROFILE_FILES_PATH) + "VP_LUNARG_desktop_baseline.json";
     const Json::Value json_document2 = ParseJsonFile(path.c_str());
     EXPECT_TRUE(validator.Check(json_document2));
 }
