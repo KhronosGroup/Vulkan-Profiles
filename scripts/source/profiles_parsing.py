1b2d2d5aa901b01ae650954525d24e768fcb42a0
Christophe <christophe@lunarg.com>
2026-06-16 17:02:50 +0200

    profiles: Strip duplicated extensions

--- scripts/source/profiles_parsing.py
+++ scripts/source/profiles_parsing.py
@@ -19,16 +19,103 @@
 # Authors: 
 # - Christophe Riccio <christophe@lunarg.com>
 
+import logging
 import os
 import json
 from pathlib import Path
 
+def _validate_profiles_json_data(json_data, schema_data) -> bool:
+    try:
+        import jsonschema
+        jsonschema.validate(json_data, schema_data)
+        return True
+    except jsonschema.exceptions.ValidationError as e:
+        logging.info(f"Message: {e.message}")
+        logging.info(f"Location in JSON: {' -> '.join([str(p) for p in e.path])}")
+        logging.info(f"Invalid value/context: {e.instance}")
+        return False
+    except jsonschema.exceptions.SchemaError as e:
+        logging.error(f"The provided schema itself is invalid: {e.message}")
+        return False
+    except ModuleNotFoundError:
+        logging.warning("`jsonschema` module is not installed, schema validation skip")
+        return False
+
+def validate_profiles_json(json_data_path: Path, json_schema_path: Path) -> bool:
+    schema_data = load_schema_json(json_schema_path)
+    if schema_data is None:
+        logging.error(f"[ERROR] Invalid profile file: {json_schema_path}")
+        return False
+
+    json_data = load_profiles_json(json_data_path)
+    if json_data is None:
+        logging.debug(f"[DEBUG] Invalid profile file: {json_data_path}")
+        return False
+
+    logging.info(f"Validating profile file: {json_data_path} with {json_schema_path}")
+
+    return _validate_profiles_json_data(json_data, schema_data)
+
+
+def validate_profiles_jsons(json_data_dir: Path, json_schema_path: Path) -> int:
+    schema_data = load_schema_json(json_schema_path)
+    if schema_data is None:
+        logging.error(f"Invalid profile file: {json_schema_path}")
+        return 0
+    
+    profiles_files_paths = []
+    for pos_json in os.listdir(json_data_dir):
+        if pos_json.endswith('.json'):
+            full_path = json_data_dir / pos_json
+            profiles_files_paths.append(full_path)
+            
+    result = 0
+    for i in range(len(profiles_files_paths)):
+        json_data = load_profiles_json(profiles_files_paths[i])
+        if json_data == None:
+            logging.debug(f"Invalid profile file: {profiles_files_paths[i]}")
+            continue
+        
+        if _validate_profiles_json_data(json_data, schema_data):
+            result += 1
+            
+    return result
+
+
+def load_schema_json(input_file):
+    with open(input_file, "r", encoding="utf-8") as file:
+        schema_file_data = json.load(file)
+
+        # Check the schema start with "http://json-schema.org/draft-07/schema#", otherwise it's not a profiles file (or a valid one)
+        if isinstance(schema_file_data, dict) and "$schema" in schema_file_data:
+            schema_url = schema_file_data["$schema"]
+            id_url = schema_file_data["$id"]
+
+            if isinstance(schema_url, str) and schema_url.startswith("http://json-schema.org/draft-07/schema#"):
+                if isinstance(id_url, str) and id_url.startswith("https://schema.khronos.org/vulkan/profiles-0."):
+                    logging.debug(f"Loading schema: {input_file}")
+                    return schema_file_data
+
+
+def load_profiles_json(input_file):
+    with open(input_file, "r", encoding="utf-8") as file:
+        json_file_data = json.load(file)
+
+        # Check the schema start with "https://schema.khronos.org/vulkan/profiles-0.", otherwise it's not a profiles file (or a valid one)
+        if isinstance(json_file_data, dict) and "$schema" in json_file_data:
+            schema_url = json_file_data["$schema"]
+
+            if isinstance(schema_url, str) and schema_url.startswith("https://schema.khronos.org/vulkan/profiles-0."):
+                logging.debug(f"Loading: {input_file}")
+                return json_file_data
+
+
 def load_profiles_jsons(input_dir):
     if not isinstance(input_dir, Path):
-        print('ERROR: No `input_dir` is not a Path type')
+        logging.error('`input_dir` is not a Path type')
         exit()
     if input_dir is None:
-        print('ERROR: No input directory set, use --input')
+        logging.error('No input directory set, use --input')
         exit()
 
     profiles_files_paths = []
@@ -37,35 +124,28 @@ def load_profiles_jsons(input_dir):
             full_path = input_dir / pos_json
             profiles_files_paths.append(full_path)
 
-    json_files_dict = {}
-
     # Load the json files in the directory
+    json_files_dict = {}
     for i in range(len(profiles_files_paths)):
-        with open(profiles_files_paths[i], "r", encoding="utf-8") as file:
-            json_file_data = json.load(file)
-
-            # Check the schema start with "https://schema.khronos.org/vulkan/profiles-0.8.", otherwise it's not a profiles file (or a valid one)
-            if isinstance(json_file_data, dict) and "$schema" in json_file_data:
-                schema_url = json_file_data["$schema"]
-
-                if isinstance(schema_url, str) and schema_url.startswith("https://schema.khronos.org/vulkan/profiles-0.8"):
-                    print(f"[DEBUG] Loading: {profiles_files_paths[i]}")
-                    json_files_dict[profiles_files_paths[i]] = json_file_data
+        json_file_data = load_profiles_json(profiles_files_paths[i])
+        if json_file_data:
+            json_files_dict[profiles_files_paths[i]] = json_file_data
 
     return json_files_dict
 
-def save_profiles_jsons(output_dir, json_files_dict):
+
+def save_profiles_jsons(json_files_dict, output_dir):
     if not isinstance(output_dir, Path):
-        print('ERROR: `output_dir` is not a Path type')
+        logging.error('`output_dir` is not a Path type')
         exit()
     if output_dir is None:
-        print('ERROR: No output directory set, use --output')
+        logging.error('No output directory set, use --output')
         exit()
     if not output_dir.exists():
-        print(f"ERROR: {output_dir} doesn't exist")
+        logging.error(f"{output_dir} doesn't exist")
         exit()
     if not output_dir.is_dir():
-        print(f'ERROR: {output_dir} is not a directory')
+        logging.error(f'{output_dir} is not a directory')
         exit()
     
     for key, value in json_files_dict.items():
--- scripts/tests/test_json.py
+++ scripts/tests/test_json.py
@@ -20,10 +20,14 @@
 # - Christophe Riccio <christophe@lunarg.com>
 
 import unittest
+import logging
 
 from pathlib import Path
 
 from ..source.profiles_parsing import load_profiles_jsons
+from ..source.profiles_parsing import save_profiles_jsons
+from ..source.profiles_parsing import validate_profiles_json
+from ..source.profiles_parsing import validate_profiles_jsons
 
 class TestJsonMethods(unittest.TestCase):
     def test_load_profiles_jsons(self):
@@ -31,10 +35,50 @@ class TestJsonMethods(unittest.TestCase):
         profiles_files_dir = repository_path / "profiles"
 
         jsons = load_profiles_jsons(profiles_files_dir)
-
         self.assertEqual(len(jsons), 8)
+
+    def test_save_profiles_jsons(self):
+        repository_path = Path(__file__).resolve().parent.parent.parent
+        profiles_files_dir = repository_path / "profiles"
+
+        jsons_ref = load_profiles_jsons(profiles_files_dir)
+        self.assertEqual(len(jsons_ref), 8)
+
+        save_profiles_jsons(jsons_ref, repository_path / "build")
+        
+        jsons_copy = load_profiles_jsons(repository_path / "build")
+        self.assertEqual(len(jsons_copy), 8)
+        
+        for ref_key in jsons_ref.keys():
+            filename = Path(ref_key).name
+            
+            copy_key = None
+            for search_key in jsons_copy.keys():
+                if search_key.name == filename:
+                    copy_key = search_key
+                    break
+
+            self.assertEqual(jsons_ref[ref_key], jsons_copy[copy_key])
+
+    def test_validate_profiles_json(self):
+        repository_path = Path(__file__).resolve().parent.parent.parent
+        profiles_files_file = repository_path / "profiles/VP_KHR_roadmap.json"
+        profiles_schema_file = repository_path / "schema/profiles-0.8-latest.json"
+
+        validated_json = validate_profiles_json(profiles_files_file, profiles_schema_file)
+        self.assertEqual(validated_json, True)
+
+    def test_validate_profiles_jsons(self):
+        repository_path = Path(__file__).resolve().parent.parent.parent
+        profiles_files_dir = repository_path / "profiles"
+        profiles_schema_file = repository_path / "schema/profiles-0.8-latest.json"
+
+        validated_jsons = validate_profiles_jsons(profiles_files_dir, profiles_schema_file)
+        self.assertEqual(validated_jsons, 8)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.FATAL, format='%(levelname)s: %(message)s')
+    
     unittest.main()
 
