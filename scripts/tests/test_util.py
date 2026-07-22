2cf9933c066571e04aa535faf6657244430cc7b6
Christophe <christophe@lunarg.com>
2026-07-22 18:22:46 +0200

    profiles: Test C.I.

--- scripts/tests/test_util.py
+++ scripts/tests/test_util.py
@@ -44,6 +44,7 @@ class TestStringMethods(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
