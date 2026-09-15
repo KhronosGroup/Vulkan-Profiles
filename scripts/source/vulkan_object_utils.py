8864da0691671a227e5e6ff0c0a987dc82fb7d8b
Christophe <christophe@lunarg.com>
2026-09-15 12:45:03 +0200

    vkprofiles: Fix extension aliases pulling

--- scripts/source/vulkan_object_utils.py
+++ scripts/source/vulkan_object_utils.py
@@ -673,7 +673,7 @@ def gatherDependentExtensions(vk: VulkanObject, version: VK_VERSION, ignore_exte
     
     for extension in extensions:
         if extension not in vk.extensions:
-            logging.error(f'{extension} is part of vk.xml, discarding')
+            logging.error(f'{extension} is not part of vk.xml, discarding')
             continue
         
         extension_data = vk.extensions[extension]
